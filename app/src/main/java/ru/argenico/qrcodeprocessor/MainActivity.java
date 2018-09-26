693b5b758f3d7bdb3dafe20edec48e172acbb399
DevastArgenico <41001953+DevastArgenico@users.noreply.github.com>
2018-09-26 15:40:34 +0200

    Initial commit

---
+++ app/src/main/java/ru/argenico/qrcodeprocessor/MainActivity.java
@@ -0,0 +1,30 @@
+package ru.argenico.qrcodeprocessor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("ico");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.tv_1);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
